33bf5ac75621211d44be889d5f055a2926c60902
lunix01 <646696730@qq.com>
2016-04-15 15:05:55 +0800

    Update atab.js

--- atab.js
+++ atab.js
@@ -1,11 +1,18 @@
 function tab(groupname, mode, activeClass) {
-  var $content = $('[data-groupname="' + groupname + '"][data-tab="content"]');
-  $content.not(':first').hide();
-  $('html, body').on(mode || 'click', '[data-groupname="' + groupname + '"][data-tab="item"]', function() {
-      var index = $('[data-groupname="' + groupname + '"][data-tab="item"]').index(this);
-      $(this).addClass(activeClass || 'active');
-      $('[data-groupname="' + groupname + '"][data-tab="item"]').not($('[data-groupname="' + groupname + '"][data-tab="item"]').eq(index)).removeClass(activeClass || 'active');
-      $content.eq(index).show();
-      $content.not($content.eq(index)).hide();
-  });
+    var $item = $('[data-groupname="' + groupname + '"][data-tab="item"]');
+    var $content = $('[data-groupname="' + groupname + '"][data-tab="content"]');
+    var $itemActive = $item.filter('.' + activeClass + '');
+    if($itemActive.length == 1) {
+        var index = $item.index($itemActive);
+        $content.not($content.eq(index)).hide();
+    } else {
+        $content.not(':first').hide();
+    }
+    $('html, body').on(mode || 'click', '[data-groupname="' + groupname + '"][data-tab="item"]', function() {
+        var index = $item.index(this);
+        $(this).addClass(activeClass || 'active');
+        $item.not($item.eq(index)).removeClass(activeClass || 'active');
+        $content.eq(index).show();
+        $content.not($content.eq(index)).hide();
+    });
 }
