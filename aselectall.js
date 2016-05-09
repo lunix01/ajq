6d5b4be765be7592f931fa7d5afc5844bdf2f96d
lunix01 <646696730@qq.com>
2016-05-09 17:03:05 +0800

    Create aselectall.js

---
+++ aselectall.js
@@ -0,0 +1,23 @@
+function selectAll(btn, chk) {
+    $btn = $(btn);
+    $btn.on('click', function() {
+        if($(this).is(':checked')) {
+            $(chk).prop('checked', 'checked');
+        } else {
+            $(chk).prop('checked', '');
+        }
+    });
+    $('html, body').on('click', chk, function() {
+        var chkArray = [];
+        $(chk).each(function() {
+            if($(this).is(':checked')) {
+                chkArray.push($(this));
+            }
+        });
+        if($(chk).length == chkArray.length) {
+            $btn.prop('checked', 'checked');
+        } else {
+            $btn.prop('checked', '');
+        }
+    });
+}
